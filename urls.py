2e6189ef4ff50fc85ac5ffd3cb8a967388bea393
Cory Zue <czue@dimagi.com>
2017-06-18 14:52:20 +0200

    delete comment

--- urls.py
+++ urls.py
@@ -1,11 +1,7 @@
 from django.conf.urls import url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
-#from django.conf import settings
-#from django.views.generic.simple import direct_to_template
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+
 
 urlpatterns = [
     url(r'^json_workbook/', views.json_workbook),
