0681f5667a87b738c20e75e0566dbda8c71daa6d
Cory Zue <czue@dimagi.com>
2017-06-18 14:52:07 +0200

    use 1.11 style urls and views

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from . import views
 #from django.conf import settings
 #from django.views.generic.simple import direct_to_template
 # Uncomment the next two lines to enable the admin:
@@ -7,9 +8,9 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 # admin.autodiscover()
 
 urlpatterns = [
-    url(r'^json_workbook/', 'pyxform_interface.views.json_workbook'),
-    url(r'^$', 'pyxform_interface.views.index'),
-    (r'^downloads/(?P<path>.*)$', 'pyxform_interface.views.serve_file'),
+    url(r'^json_workbook/', views.json_workbook),
+    url(r'^$', views.index),
+    url(r'^downloads/(?P<path>.*)$', views.serve_file),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
